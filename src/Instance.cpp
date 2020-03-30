ded1c00f1eb861ef59a7bea6808b11812f155a71
franklin <franklinlalere@gmail.com>
2020-03-30 12:12:00 +0200

    Ajout de la classe Instance

--- src/Instance.cpp
+++ src/Instance.cpp
@@ -5,9 +5,11 @@ Instance::Instance(SDL_Renderer *renderer) : m_position(500, 500, 500, 500), m_d
 {
 	Image *houseIm = new Image("../data/images/House.png", renderer);
 	m_instanceMap.push_back(houseIm);
+	m_chargeInterieur = false;
+	m_doorInInstance = Rectangle(20, 0, 30, 70);
 }
 
-Instance::Instance(const Rectangle &position, const Rectangle &door, SDL_Renderer *renderer)
+Instance::Instance(const Rectangle &position, const Rectangle &door, Vec2<unsigned int> winDim, SDL_Renderer *renderer)
 {
 	if (renderer == NULL)
 	{
@@ -17,23 +19,110 @@ Instance::Instance(const Rectangle &position, const Rectangle &door, SDL_Rendere
 	}
 	m_position.rectangle = position.rectangle;
 	m_door.rectangle = door.rectangle;
+	//apparition dans l'instance en bas au centre de l'écran
+	m_doorInInstance = Rectangle(300, 430, 40, 40);
 
-	Vec2<unsigned int> winDim(1024, 768);
 	Image *houseIm = new Image("../data/images/House.png", position, winDim, renderer);
+	Image *doorIm = new Image("../data/images/door.png", m_door, winDim, renderer);
 	m_instanceMap.push_back(houseIm);
+	m_instanceMap.push_back(doorIm);
+	m_chargeInterieur = false;
 
+	//ennemi (characters)
+	Character *enemi;
+	//4 clone du même enemi mais seront indépendant
+	int EnnemisPositions[][2] = {
+		{40, 150},
+		{60, 50 },
+		{160, 300}
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		enemi = new Character(Rectangle(EnnemisPositions[i][0], EnnemisPositions[i][1], 40, 40), 100, "../data/Enemy.png", renderer, 10);
+		m_ennemis.push_back(enemi);
+	}
 }
 
 Instance::~Instance()
 {
-	for (auto im = m_instanceMap.begin(); im != m_instanceMap.end(); ++im)
+	for (auto i = m_instanceMap.begin(); i != m_instanceMap.end(); ++i)
 	{
-		Image *image = *im;
+		Image *image = *i;
 		delete image;
+	}
+
+	for (auto i = m_ennemis.begin(); i != m_ennemis.end(); ++i)
+	{
+		Character *enemi = *i;
+		delete enemi;
 	}
 }
 
 void Instance::display(SDL_Renderer *renderer)
 {
-	m_instanceMap[0]->display(renderer);
+	if (m_chargeInterieur)
+	{
+		//std::cout << "doit charger l'intérieur" << std::endl;
+		//std::cout << "charge interieur" << std::endl;
+		SDL_Rect rectangle = Rectangle(0, 0, 640, 480).rectangle;
+		SDL_RenderFillRect(renderer, &rectangle);
+
+		//affiche de la porte de sortie
+		m_instanceMap[1]->display(renderer);
+		//SDL_RenderFillRects(SDL_Renderer *renderer, const SDL_Rect *rects, int count)
+		//affiche des ennemis
+		for (long unsigned int i = 0; i < m_ennemis.size(); i++)
+		{
+			m_ennemis[i]->display(renderer);
+			m_ennemis[i]->setLoaded(true);
+		}
+	}
+	else
+	{
+		//affiche d'une instance l'extérieur + la porte
+		m_instanceMap[0]->display(renderer);
+		m_instanceMap[1]->display(renderer);
+
+		for (long unsigned int i = 0; i< m_ennemis.size(); i++)
+		{
+			m_ennemis[i]->setLoaded(false);
+		}
+	}
 }
+
+void Instance::TestPlayerTakeDoor(Player &p)
+{
+	Rectangle rectPlayer = p.getPos();
+	Rectangle TeleportPosition;
+	if (rectPlayer.in(m_door) && !m_chargeInterieur)
+	{
+		std::cout << "position du joueur : " << rectPlayer.rectangle.x << ", " << rectPlayer.rectangle.y << std::endl;
+		std::cout << "passe la porte de l'extérieur" << std::endl;
+		
+		m_instanceMap[1]->move(m_doorInInstance, {640, 480});
+
+		TeleportPosition = Rectangle(m_doorInInstance.rectangle.x, m_doorInInstance.rectangle.y - 50, rectPlayer.rectangle.w, rectPlayer.rectangle.h);
+		//téléportation du joueur vers une position proche de la porte intérieur de l'instance
+		p.setPos(TeleportPosition);
+		m_chargeInterieur = true;
+	}
+
+	//si le joueur quitte sort par la porte de l'instance
+	if (m_chargeInterieur && rectPlayer.in(m_doorInInstance))
+	{
+
+		std::cout << "position du joueur : " << rectPlayer.rectangle.x << ", " << rectPlayer.rectangle.y << std::endl;
+		std::cout << "passe la porte de l'intérieur" << std::endl;
+
+		m_instanceMap[1]->move(m_door, {640, 480});
+		
+		
+		TeleportPosition = Rectangle(m_door.rectangle.x, m_door.rectangle.y - 45, rectPlayer.rectangle.w, rectPlayer.rectangle.h);
+		std::cout << "le personnage a été téléloporté de l'intérieur vers l'extérieur" << std::endl;
+		p.setPos(TeleportPosition);
+
+		m_chargeInterieur = false;
+	}
+}
+
--- src/Instance.h
+++ src/Instance.h
@@ -4,6 +4,64 @@
 #include <vector>
 #include "Rectangle.h"
 #include "Image.h"
+#include "Character.h"
+
+class Player
+{
+public:
+	Player(const Image &im)
+	{
+		m_position = {200, 140};
+		m_imcharacter = im;
+	}
+
+	void moove(int direction)
+	{
+		Rectangle pos;
+		//up
+		if (direction == 0)
+		{
+			m_position.y -= 3;
+		}
+
+		//bas
+		if (direction == 1)
+		{
+			m_position.y += 3;
+		}
+
+		//gauche
+		if (direction == 2)
+		{
+			m_position.x -= 3;
+		}
+
+		//droite
+		if (direction == 3)
+		{
+			m_position.x += 3;
+		}
+
+		pos = Rectangle(m_position.x, m_position.y, 50, 50);
+		m_imcharacter.move(pos, {640, 480});
+	}
+
+	Rectangle getPos() { return Rectangle(m_position.x, m_position.y, 30, 50); }
+
+	void setPos(const Rectangle &position)
+	{
+		m_imcharacter.move(position, {640, 480});
+	}
+
+	void display(SDL_Renderer *renderer)
+	{
+		m_imcharacter.display(renderer);
+	}
+
+private:
+	Vec2<int> m_position;
+	Image m_imcharacter;
+};
 
 /**
  * @brief     La classe Instance représente une maison ou un dongeon
@@ -11,55 +69,32 @@
 class Instance
 {
 	public:
-		/**
-		 * @brief      Construit une nouvelle instance
-		 *
-		 * @param      renderer  Le rendu
-		 */
+		//créé une nouvelle instance (maison ou dongeon)
 		Instance(SDL_Renderer *renderer);
-
-		/**
-		 * @brief      Construit une nouvelle instance
-		 *
-		 * @param[in]  position  position sur l'écran
-		 * @param[in]  door      position de la porte sur l'écran
-		 * @param      renderer  Le rendu
-		 */
-		Instance(const Rectangle &position, const Rectangle &door, SDL_Renderer *renderer);
-
-		/**
-		 * @brief      Détruit l'objet.
-		 * 
-		 * Désalloue la mémire pour les images
-		 */
+		//crée une nouvelle instance de dongeon
+		Instance(const Rectangle &position, const Rectangle &door, Vec2<unsigned int> winDim, SDL_Renderer *renderer);
+		//détruit l'instance
 		~Instance();
-
-		/**
-		 * @brief      Accesseur de m_door
-		 *
-		 * @return     Le rectangle m_door
-		 */
+		//test si le joueur passe la porte
+		void TestPlayerTakeDoor(Player &p);
+		//retourne le rectangle représentant la porte
 		Rectangle getDoor() const { return m_door; }
-
-		/**
-		 * @brief      Accesseur de m_position
-		 *
-		 * @return     Le rectangle m_position
-		 */
+		//retourne le rectangle représentant l'instance
 		Rectangle getPos() const { return m_position; }
-
-		/**
-		 * @brief      Affiche l'instance
-		 *
-		 * @param      renderer  Le rendu
-		 */
+		//procédure d'affichage de l'instance
 		void display(SDL_Renderer *renderer);
 
 	
 	private:
+		//void charge(const Map &map, SDL_Renderer *renderer);
+
+		std::vector<Character*> m_ennemis;
+
 		Rectangle m_position;
 		Rectangle m_door;
+		Rectangle m_doorInInstance;
 		std::vector<Image*> m_instanceMap;
+		bool m_chargeInterieur;
 
 		
 };
